d7418319bb84b0f90c630c82ba2d8301ec6a2983
qubicbit <sis.esse161@outlook.com>
2025-09-10 12:36:37 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace githubAction_demo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
